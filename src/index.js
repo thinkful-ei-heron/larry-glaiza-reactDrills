32c5dead821133ff9945c55ce63fca93f54dac5a
Glaiza Wagner <glaizawagner@gmail.com>
2019-10-09 11:55:08 -0700

    Roulette Gun completed

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import RouletteGun from './state-drills/RouletteGun';
+
 // import HelloWorld from './state-drills/HelloWorld';
-import Bomb from './state-drills/Bomb';
+// import Bomb from './state-drills/Bomb';
 
 // ReactDOM.render(<HelloWorld />, document.getElementById('root'));
-ReactDOM.render(<Bomb />, document.getElementById('root'));
+// ReactDOM.render(<Bomb />, document.getElementById('root'));
+ReactDOM.render(<RouletteGun />, document.getElementById('root'));
---
+++ src/state-drills/RouletteGun.js
@@ -0,0 +1,53 @@
+/* eslint-disable no-useless-constructor */
+import React, { Component } from 'react'
+
+export class RouletteGun extends Component {
+    state = {
+        chamber: null,
+        spinningTheChamber: false
+    }
+
+    static defaultProp = {
+        bulletInChamber: 8
+    }
+    
+
+    displayParagraph() {
+        if (this.state.spinningTheChamber) {
+            return 'spinning the chamber and pulling the trigger!'
+        } else if(this.state.chamber === this.state.bulletInChamber){
+            return 'BANG'
+        } else {
+            return  `You're safe`
+        }
+    }
+
+    handleClick = () => {
+        this.setState({
+            spinningTheChamber: true
+        });
+
+        this.timeOut = setTimeout( () => {
+            const timeOutNewValue = Math.ceil(Math.random() * 8)
+                this.setState({
+                    chamber: timeOutNewValue
+                    , spinningTheChamber: false
+                })
+        }, 2000)
+    }
+
+    componentWillUnmount() {
+        clearTimeout(this.timeOut)
+    }
+
+    render() {
+        return (
+            <div>
+                <p>{this.displayParagraph()}</p>
+                <button onClick={this.handleClick}>Pull the trigger!</button>
+            </div>
+        )
+    }
+}
+
+export default RouletteGun
